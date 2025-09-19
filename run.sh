bd1d49ec0bdbd727d14c60ba2631d84da4402953
caropio <carolinepioger98@gmail.com>
2025-09-19 12:13:29 +0200

    update

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+cd /Users/carolinepioger/Desktop/cirp-group-1-project
 python -m http.server &
-open -a "Google Chrome" http://localhost:8000/
+open -a "Google Chrome" http://localhost:8000/index.html
